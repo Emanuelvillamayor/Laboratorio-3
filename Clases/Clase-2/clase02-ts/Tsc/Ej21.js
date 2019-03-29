d7b491daf1b484d345069c524a254d3f5178e145
alumno <alumno@utnfra.com.ar>
2019-03-29 12:11:19 -0300

    clase 2 finalizad

---
+++ Clases/Clase-2/clase02-ts/Tsc/Ej21.js
@@ -0,0 +1,15 @@
+"use strict";
+/*Ejercicio 21 :
+ Se necesita hacer una página que contenga dos cuadros de texto (dónde se ingresarán
+números), un botón (con la leyenda ‘Calcular’) y cuatro controles de tipo ‘radioButton’(va a haber uno ) (suma,
+resta, multiplicación y división). Cuando se pulsa el botón una función deberá mostrar el
+resultado (en la consola y en otro cuadro de texto), de acuerdo al tipo de operación que el
+usuario eligió. Utilizar la estructura ‘switch’
+*/
+function ResultadoOperacion() {
+    var valor1 = document.getElementById("txtValor1").value;
+    var valor2 = document.getElementById("txtValor2").value;
+    // let rdButton : NodeListOf(HTMLElement) =  document.getElementsByName("txtValor2");
+    //let opcion : string[]=(<> document.getElementsByName("txtValor2")).value;
+}
+//# sourceMappingURL=Ej21.js.map
