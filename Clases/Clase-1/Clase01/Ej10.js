2d2278d70acc0a2e4e5877fae691e7fecb3d8b4d
Your Name <you@example.com>
2019-03-28 21:22:13 -0300

    Ejercicio 10 terminado

--- Clases/Clase-1/Clase01/Ej10.js
+++ Clases/Clase-1/Clase01/Ej10.js
@@ -1,14 +1,28 @@
-"use strict";
 /*Ejercicio 10:
 Definir una función que muestre información sobre una cadena de texto que se le pasa
 como argumento. A partir de la cadena que se le pasa, la función determina si esa cadena
 está formada sólo por mayúsculas, sólo por minúsculas o por una mezcla de ambas.
 */
-function AnalizarCadena(cadena) {
-    for (var _i = 0, cadena_1 = cadena; _i < cadena_1.length; _i++) {
-        var letra = cadena_1[_i];
-        console.log(letra + "\n");
+//creo funcion
+function ValidarCadena(cadena) {
+    if (cadena === cadena.toUpperCase()) {
+        console.log("Formado solo por mayusculas\n");
+    }
+    else if (cadena === cadena.toLowerCase()) {
+        console.log("Formado solo por minusculas\n");
+    }
+    else {
+        console.log("Formado por mayusculas y minusculas\n");
     }
 }
-AnalizarCadena("hola");
-//# sourceMappingURL=Ej10.js.map
+var cadena1 = "HOLA";
+var cadena2 = "hola";
+var cadena3 = "HoLa";
+//Implemento funcion
+console.log("Pruebo con una cadena en MAYUSCULA:\n");
+ValidarCadena(cadena1);
+console.log("Pruebo con una cadena en minuscula:\n");
+ValidarCadena(cadena2);
+console.log("Pruebo con una cadena en MAYUSCULA y minuscula:\n");
+ValidarCadena(cadena3);
+console.log();
